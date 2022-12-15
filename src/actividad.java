8dd98f830eda00f020564d809c16a11aa4204752
Alejandro <alefermar16@alu.edu.gva.es>
2022-12-15 17:56:56 +0100

    [MOD] mostrar numeros hasta el 25

--- src/actividad.java
+++ src/actividad.java
@@ -1,6 +1,6 @@
 public class actividad{
     public static void main(String[] args) {
-        for (int i = 1; i <=20 ; i++) {
+        for (int i = 1; i <=25 ; i++) {
             System.out.printf(i + ", ");
         }
     }
